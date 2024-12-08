dd330d8b4261748c2b4509d9dfc4d8fc94e25265
rendee <rendeelloren@gmail.com>
2024-12-09 01:39:19 +0800

    code cleanup

--- src/NexusDigital.EntityFrameworkCore/EntityFrameworkCore/NexusDigitalDbContextConfigurer.cs
+++ src/NexusDigital.EntityFrameworkCore/EntityFrameworkCore/NexusDigitalDbContextConfigurer.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using System.Data.Common;
 
 namespace NexusDigital.EntityFrameworkCore;
@@ -7,11 +8,13 @@ public static class NexusDigitalDbContextConfigurer
 {
     public static void Configure(DbContextOptionsBuilder<NexusDigitalDbContext> builder, string connectionString)
     {
+        builder.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
         builder.UseSqlServer(connectionString);
     }
 
     public static void Configure(DbContextOptionsBuilder<NexusDigitalDbContext> builder, DbConnection connection)
     {
+        builder.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
         builder.UseSqlServer(connection);
     }
 }
--- src/NexusDigital.EntityFrameworkCore/Migrations/20241208163726_InitialDatabaseSetup.Designer.cs
+++ src/NexusDigital.EntityFrameworkCore/Migrations/20241208163726_InitialDatabaseSetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NexusDigital.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using NexusDigital.EntityFrameworkCore;
 namespace NexusDigital.Migrations
 {
     [DbContext(typeof(NexusDigitalDbContext))]
-    partial class NexusDigitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241208163726_InitialDatabaseSetup")]
+    partial class InitialDatabaseSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
